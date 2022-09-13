509b0e34db6498b6b8effb34914aabd0209963b7
eduardo <tato87911@gmail.com>
2022-09-12 22:23:08 -0600

    Agregar archivos de proyecto.

---
+++ datos y calcular/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace datos_y_calcular
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textnum1 = new System.Windows.Forms.TextBox();
+            this.textnum2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mostrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(213, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Sumar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(394, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Mostrar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textnum1
+            // 
+            this.textnum1.Location = new System.Drawing.Point(213, 30);
+            this.textnum1.Name = "textnum1";
+            this.textnum1.Size = new System.Drawing.Size(100, 20);
+            this.textnum1.TabIndex = 4;
+            // 
+            // textnum2
+            // 
+            this.textnum2.Location = new System.Drawing.Point(213, 72);
+            this.textnum2.Name = "textnum2";
+            this.textnum2.Size = new System.Drawing.Size(100, 20);
+            this.textnum2.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Opcion 1",
+            "Opcion 2",
+            "Opcion 3",
+            "Opcion 4",
+            "Opcion 5"});
+            this.comboBox1.Location = new System.Drawing.Point(394, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 243);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textnum2);
+            this.Controls.Add(this.textnum1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Pracica form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textnum1;
+        private System.Windows.Forms.TextBox textnum2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ datos y calcular/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace datos_y_calcular
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola es mi primer fomulario");
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string mensaje = textBox1.Text;
+            MessageBox.Show(mensaje);
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(textnum1.Text);
+            double num2 = double.Parse(textnum2.Text);
+            var suma = num1 + num2;
+            MessageBox.Show(suma.ToString());
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            string itemSelected = comboBox1.Text;
+            MessageBox.Show(itemSelected);
+        }
+
+    }
+    
+}
